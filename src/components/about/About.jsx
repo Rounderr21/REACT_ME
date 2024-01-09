d16614aa550ebe8e2121e51aa6a43b1a1ca38fb8
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-08 17:13:25 -0800

    updated about css and jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,7 @@
-import coverMain from "../../assets/Images/bloodSworn-issue1-coverMain-colors-v3.jpg";
 import pg23 from "../../assets/Images/bloodSworn-issue1-pg23-colors.jpg";
 import pg15 from "../../assets/Images/bloodSworn-issue1-pg15-colors.jpg";
 import company from "../../assets/Images/BloodHaven_Productions.png";
 import pg24 from "../../assets/Images/bloodSworn-issue1-pg24-colors-v3.jpg";
-import pg13 from "../../assets/Images/bloodSworn-issue1-pg13-colors .jpg";
 import pg18 from "../../assets/Images/bloodSworn-issue1-pg18-colors.jpg";
 import "./about.scss";
 
@@ -14,14 +12,20 @@ export default function About() {
         // Wrap JSX expressions inside a single parent element
         <div>
             <div className="Container">
-                <div id="photoCompany">
-                    <h1>More about me</h1>
-                    <p id="paragraph">Software development is not just a job; it is my passion. I relish the opportunity to tackle complex challenges 
+                <div className="photoCompany">
+                    <p id="paragraph1">Software development is not just a job; it is my passion. I relish the opportunity to tackle complex challenges 
                         and bring innovative solutions to life. The thrill of creating functional, elegant software is what drives me 
                         every day.From cutting-edge frameworks and languages to tried-and-true tools, I thrive on staying up-to-date 
                         with the ever-evolving tech landscape. Expect to see a diverse skill set that includes but is not limited to 
                         JavaScript, Python, React, Node.js, and more.
                     </p>
+                    <div className="picture">
+                        <img className="comic" src={pg23} alt="comic book" />
+                        <img className="comic" src={pg15} alt="comic book" />
+                        <img className="comic" src={company} alt="company" />
+                        <img className="comic" src={pg24} alt="comic book" />
+                        <img className="comic" src={pg18} alt="comic book" />
+                    </div>
                     <p id="paragraph2">While my career highlights undoubtedly serve as the primary point of interest, 
                         I would like to take a moment to provide insight into my personal endeavors. As a passionate enthusiast, 
                         I embarked on an entrepreneurial venture known as BloodHaven Productions. My deep-seated affinity for the art of storytelling, 
@@ -29,21 +33,8 @@ export default function About() {
                         an ongoing project titled &quot;BloodSworn.&quot; My dedication to this artistic pursuit mirrors the same unwavering commitment 
                         I exhibit in the realm of coding, both receiving my full and undivided attention until the desired level of perfection is attained.
                     </p>
-                    <div className="picture">
-                        <img className="comic" src={coverMain} alt="comic book" />
-                        <img className="comic" src={pg23} alt="comic book" />
-                        <img className="comic" src={pg15} alt="comic book" />
-                        <img className="comic" src={company} alt="company" />
-                        <img className="comic" src={pg24} alt="comic book" />
-                        <img className="comic" src={pg13} alt="comic book" />
-                        <img className="comic" src={pg18} alt="comic book" />
-                    </div>
-                    <p id="thanks">Thank you for reading my journey, click on the projects button below to see my work.</p>
+                    <p id="thanks">Thank you for reading my journey, click on the projects button in the navbar to see my work.</p>
                 </div>
-            </div>
-
-            <div className="grid-item grid-item-3">
-                <div id="buttonProject"><button id="button">Projects</button></div>
             </div>
         </div>
     );
