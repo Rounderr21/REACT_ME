5a3ca9ae832dc544797eaea59131fd0c0da43b0b
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-08 17:15:51 -0800

    updated resume jsx and created css

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,15 +1,27 @@
-import ResumeImg from "../../assets/Images/ResumeImg.png"
-import "./resume.scss"
+import ResumeImg from "../../assets/Images/ResumeImg.png";
+import "./resume.scss";
 
 export default function Resume() {
-return (
+  return (
     <div className="resumePage">
-        <div>
-            <h1>Resume</h1>
-            <p>Presented here is a sample of my professional resume. Feel free to click on the image to access the comprehensive version of my credentials and experience</p>
-            <a href="https://docs.google.com/document/d/1AYQGocHNYthJoegaORFkPeB7ow2b9Vhy/edit?usp=sharing&ouid=113693766888778622789&rtpof=true&sd=true"> 
-            <img src={ResumeImg} alt="resume" /></a>
+      <div className="resumeContent">
+        <div className="textSection">
+          <h1>Welcome!</h1>
+          <p>Enclosed is a snapshot of my professional resume. For a comprehensive overview of my credentials and experience, please click on the image provided.</p>
+          <ul>
+            <ol className="skills">Skills:</ol>
+            <ol>Programming Languages: Node.js, REACT, Express, Python, HTML, CSS, JavaScript</ol>
+            <ol>Database: MySQL, MongoDB</ol>
+            <ol>Frameworks & Libraries: Bootstap, Tailwind, jQuery, Handlebars</ol>
+            <ol>API's & Technlology: GraphQL, Heroku</ol>
+          </ul>
         </div>
+        <div className="imageSection">
+          <a href="https://docs.google.com/document/d/1AYQGocHNYthJoegaORFkPeB7ow2b9Vhy/edit?usp=sharing&ouid=113693766888778622789&rtpof=true&sd=true"> 
+            <img src={ResumeImg} alt="resume" style={{ width: "500px", height: "550px" }} />
+          </a>
+        </div>
+      </div>
     </div>
-)
+  );
 }
