0296723914371415c2348a7bab083752f33ef4a2
Cody Schwengler01~ <cschwengler@outlook.com>
2024-01-22 21:34:02 -0800

    used emailjs to make emails go to correct spot

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,72 +1,60 @@
-import React, { useState } from 'react';
 import './contact.scss';
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [emailError, setEmailError] = useState('');
+  const reForm = useRef();
 
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    if (!name || !email || !message) {
-      alert('All fields are required');
-      return;
-    }
-
-    if (!/\S+@\S+\.\S+/.test(email)) {
-      setEmailError('Please enter a valid email address');
-      return;
-    }
-
-    const formData = { name, email, message };
-    try {
-      const response = await fetch('http://localhost:3001/api/sendEmail', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        reForm.current,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        () => {
+          alert("Message successfully sent!");
+          window.location.reload(false);
         },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        console.log('Email sent!');
-        // Handle success, e.g., show a success message
-      } else {
-        console.log(response);
-        // Handle errors
-      }
-    } catch (error) {
-      console.log(error);
-      // Handle errors
-    }
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    setEmailError('');
+        () => {
+          alert("Failed to send the message, please try again");
+        }
+      );
   };
 
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form onSubmit={handleSubmit}>
+      <form
+        ref={reForm}
+        onSubmit={sendEmail}
+        className="--form-control --card"
+      >
         <div className="input-group">
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" required />
+        <input
+          type="text"
+          placeholder="Full Name"
+          name="User_name"
+          required
+        />
+        </div>
+        <div className='input-group'>
+        <input type="Email" placeholder="Email" name="User_email" required />
+        </div>
+        <div className='input-group'>
+        <input type="text" placeholder="Subject" name="Subject" required />
         </div>
         <div className="input-group">
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            placeholder="Email"
-            required
-          />
-          {emailError && <p className="error">{emailError}</p>}
-        </div>
-        <div className="input-group">
-          <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Message" required></textarea>
+        <textarea
+          name="message"
+          placeholder="Message"
+          cols="30"
+          rows="10"
+        ></textarea>
         </div>
         <div className="input-group">
           <button type="submit">Submit</button>
